c727094c6a5549002feb2dc0c34957646748a20d
Dhanvanti Chelani <90795524+dhanvantichelani22@users.noreply.github.com>
2023-05-18 11:41:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ class Rectangle {
 	return this._height;
 }
 	getArea(){
-		return (this._width)*(this._height);
+		return this._width*this._height;
 }
 }
 
@@ -20,7 +20,7 @@ class Square extends Rectangle{
 		super(side,side);
 }
 getPerimeter(){
-		return 2*(this._width) + 2*(this._height);
+		return 2*this._width+ 2*this._height
 }
 
 }
